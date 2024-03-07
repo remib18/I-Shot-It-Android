9c1dd567fec572bdd1e5503fa4ad264756e03c99
Rémi Bernard <remib18@outlook.fr>
2024-03-07 16:14:35 +0100

    Functioning base navigation
    Signed-off-by: Rémi Bernard <remibernard@Air-de-Remi.lan>

--- app/src/main/java/com/example/ishotit/MainActivity.java
+++ app/src/main/java/com/example/ishotit/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.ishotit;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String userId = prefs.getString("userId", "");
+        String lastPosted = prefs.getString("lastPosted", null);
+
+        Class<?> activity;
+        if (userId.isEmpty()) {
+            activity = Onboarding.class;
+        } else if (Objects.nonNull(lastPosted) || !Objects.requireNonNull(lastPosted).isEmpty()) {
+            assert lastPosted != null;
+            Date lastPostedAt = new Date(Long.parseLong(lastPosted));
+            Date now = new Date();
+            boolean havePostedInTheLast24h = now.getTime() - lastPostedAt.getTime() < 24 * 60 * 60 * 1000;
+            activity = havePostedInTheLast24h ? MyPresentLife.class : Camera.class;
+        } else {
+            activity = Camera.class;
+        }
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+
+        finish();
     }
 }
