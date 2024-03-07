fddbe1c91f550db7f89615a7b2211843e42dc640
Rémi Bernard <remib18@outlook.fr>
2024-03-07 18:40:01 +0100

    Functioning reverse geocoding
    Signed-off-by: Rémi Bernard <remibernard@Air-de-Remi.lan>

---
+++ app/src/main/java/com/example/ishotit/BackendConnector/SecretManager.java
@@ -0,0 +1,9 @@
+package com.example.ishotit.BackendConnector;
+
+
+// TODO: Ensure to make the finding of the api key more difficult (see https://stackoverflow.com/questions/14570989/best-practice-for-storing-and-protecting-private-api-keys-in-applications)
+public class SecretManager {
+
+    static final String REVERSE_GEOCODING_API_KEY = "7573f29cfc6447c7ac65591347b29d06";
+
+}
--- app/src/main/java/com/example/ishotit/Camera.java
+++ app/src/main/java/com/example/ishotit/Camera.java
@@ -1,12 +1,15 @@
 package com.example.ishotit;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ishotit.BackendConnector.ReverseGeocoding;
 
 public class Camera extends AppCompatActivity {
 
@@ -20,5 +23,18 @@ public class Camera extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ReverseGeocoding.getLocationName(this, this, this::handleGeocodingResult);
+    }
+
+    private Void handleGeocodingResult(String locationName) {
+        TextView location = findViewById(R.id.camera_location);
+        location.setText(locationName);
+        return null;
     }
 }
